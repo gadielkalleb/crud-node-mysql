6615a421675718b6b18bcda83e68e31e11393112
GadielKalleb <gadiel_kalleb@hotmail.com>
2018-05-25 17:51:03 +0000

    criação dos metodos updat e create

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 const pessoas = require('./routes/pessoas')
 const projetos = require('./routes/projetos')
 const connection = mysql.createConnection({
@@ -17,6 +18,7 @@ const dependencies = {
 }
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false}))
 
 // view engine
 app.set('views', path.join(__dirname, 'views'))
--- models/pessoas.js
+++ models/pessoas.js
@@ -16,4 +16,37 @@ const deleteOne = (connection, id) => {
     })
 }
 
-module.exports = { findAll, deleteOne }
+const update = (connection, id,data) => {
+    new Promise((resolve, reject) => {
+        connection.query(`update pessoas set nome='${data.nome}', nascimento='${data.nascimento}', cargo='${data.cargo}' where id = ${id}`, (err) => {
+            if (err) reject(err)
+            else resolve()
+        })
+    })
+}
+
+const create = (connection ,data) => {
+    new Promise((resolve, reject) => {
+        connection.query(`insert into pessoas (nome, nascimento, cargo) values ('${data.nome}', '${data.nascimento}', '${data.cargo}')`, (err) => {
+            if (err) reject(err)
+            else resolve()
+        })
+    })
+}
+
+const findById = (connection, id) => {
+    return new Promise((resolve, reject) => {
+        connection.query('select * from pessoas where id = '+id, (err, result) => {
+            if (err) reject(err)
+            else {
+                if(result.length>0) {
+                    resolve(result[0])
+                } else {
+                    resolve({})
+                }
+            }
+        })
+    })
+}
+
+module.exports = { findAll, deleteOne, create, findById, update }
